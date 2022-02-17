db0f6431f976e7e0687b8b890c394ce8b0fa9dab
soohoonlee <jjinimania@gmail.com>
2022-02-17 11:17:19 +0900

    Method reference

---
+++ src/main/java/me/ssoon/java8to17/App.java
@@ -0,0 +1,28 @@
+package me.ssoon.java8to17;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class App {
+
+	public static void main(String[] args) {
+		UnaryOperator<String> hi = Greeting::hi;
+		System.out.println("hi.apply(\"Soohoon\") = " + hi.apply("Soohoon"));
+
+		Greeting greeting = new Greeting();
+		UnaryOperator<String> hello = greeting::hello;
+		System.out.println("hello.apply(\"soohoon\") = " + hello.apply("soohoon"));
+
+		Supplier<Greeting> newGreeting = Greeting::new;
+		System.out.println("newGreeting.get().hello(\"soohoon\") = " + newGreeting.get().hello("soohoon"));
+
+		Function<String, Greeting> newGreeting2 = Greeting::new;
+		System.out.println("newGreeting2.apply(\"soohoon\").getName() = " + newGreeting2.apply("soohoon").getName());
+
+		String[] names = {"dduddu", "nana", "kkukku", "yooddu"};
+		Arrays.sort(names, String::compareToIgnoreCase);
+		System.out.println("Arrays.toString(names) = " + Arrays.toString(names));
+	}
+}
